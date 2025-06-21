779cb3c3699d4e1f823ce8adedb4291b304cfa17
unknown <bhoomikagowda913@gmail.com>
2025-06-21 19:48:28 +0530

    Initial commit - Product Catalog Microservice

---
+++ src/main/java/com/javatechie/crud/example/entity/Product.java
@@ -0,0 +1,61 @@
+package com.javatechie.crud.example.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "PRODUCT_TBL")
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private int quantity;
+    private double price;
+
+    // Constructors
+    public Product() {}
+    public Product(int id, String name, int quantity, double price) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    // Setters (optional if used by JPA)
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
